0e44fa3cd9e627f20de53771b7d621ac5508fae0
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2021-04-15 12:45:19 +0200

    next

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/quizz/[id]/[question_id]":["static\u002Fchunks\u002Fpages\u002Fquizz\u002F[id]\u002F[question_id].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fquizz\u002F[id]\u002F[question_id]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
